d91fbdc3e6ce0783a1231b8ad844231b634bed5a
Victor Jacek <victorjacek@gmail.com>
2020-01-04 14:21:42 -0500

    move work to react app and github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+import { getLevel, getTime, COLORS } from './utils.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    score: 0,
+    progress: 0,
+    highscore: 0,
+    color: COLORS.LIGHTGREY,
+    gameOver: false,
+    newGameCountdown: 5
+  }
+
+  // update score, progress bar, background
+  handleClick = () => {
+
+    this.setState((prevState) => {
+
+      // Current game continues
+      if (!prevState.gameOver) {
+
+        const newScore = prevState.score + 1;
+        const { color, min, max } = getLevel(newScore);
+        const time = getTime(this.state.score, this.state.highscore);
+
+        console.log('color: ' + color + ', min: ' + min + ', max: ' + max + ', score: ' + prevState.score + ', highscore: ' + prevState.highscore + ', time: ' + time);
+
+        clearTimeout(prevState.timer);
+
+        return {
+          score: newScore,
+          progress: (newScore - min) / (max - min) * 100,
+          highscore: newScore > prevState.highscore ? newScore : prevState.highscore,
+          color: color,
+          timer: setTimeout(() => {
+            console.log('timer over');
+            this.setState({
+              gameOver: true,
+              color: COLORS.RED
+            })
+          }, time)
+        };
+      }
+      // New game
+      else {
+        // Count down to give time to realize the game is over while clicking fast
+        if (prevState.newGameCountdown > 0) {
+          return {
+            progress: 0,
+            score: prevState.newGameCountdown,
+            newGameCountdown: prevState.newGameCountdown - 1
+          };
+        }
+        // Actual new game state
+        else {
+          console.log('new game');
+          return {
+            score: 0,
+            progress: 0,
+            color: COLORS.LIGHTGREY,
+            gameOver: false,
+            newGameCountdown: 5
+          };
+        }
+      }
+    });
+
+  }
+
+  render() {
+    return (
+
+      <Container fluid
+        onClick={this.handleClick}
+        style={{
+          minHeight: "100vh",
+          border: "2px solid blue",
+          background: this.state.color
+        }}
+      >
+
+        <Row style={{ height: "20vh" }} />
+
+        <Row style={{ height: "10vh", border: "1px solid orange" }}>
+          <Col md={1} />
+          <Col md={10}>
+            <ProgressBar variant="primary" now={this.state.progress} />
+          </Col>
+          <Col md={1} />
+        </Row>
+
+        <div className="row align-items-center" style={{ border: "1px solid yellow" }}>
+          <Col />
+          <Col md={1} style={{
+            textAlign: "center",
+            border: "1px solid red",
+            fontWeight: 700,
+            fontSize: "70px"
+          }}>
+            <div>
+              {this.state.score}
+            </div>
+          </Col>
+          <Col />
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default App;
---
+++ src/utils.js
@@ -0,0 +1,43 @@
+export const COLORS = {
+  LIGHTGREY: 'lightgrey',
+  ORANGE: '#E2992C',
+  YELLOW: '#E2DF2C',
+  GREEN: '#6BE22C',
+  TEAL: '#2CE296',
+  LIGHTBLUE: '#2CE2D9',
+  BLUE: '#2C6BE2',
+  PURPLE: '#A22CE2',
+  PINK: '#E22CA5',
+  BLACK: '#000000',
+  RED: '#FF0000'
+};
+
+
+const LEVEL_STEP = 10;
+export const getLevel = (score) => {
+  const level = Math.floor(score / LEVEL_STEP);
+  return {
+    color: Object.keys(COLORS)[level],
+    min: level * LEVEL_STEP,
+    max: (level + 1) * LEVEL_STEP
+  };
+};
+
+
+// Return a number of ms driven by the current click count and high score
+// Game gets harder based on your previous highscore
+export const getTime = (score, highscore) => {
+
+  // Tuning... how fast the game gets harder
+  // Starting with 1 second as the most time possible between clicks
+  // Tuning=2 allows doubling of current high score
+  // Tuning=10 would allow 10x high score before timeout becomes too fast
+  // Starting with 1.2 so that games are almost always killed before reaching 120% of current highscore
+  var progressSpeed = 1.2;
+
+  var progress = score / (highscore === 0 ? 10 : highscore);
+  var time = (1 - progress / progressSpeed) * 1000;
+
+  // Save from being totally impossible
+  return time < 10 ? 10 : time;
+};
